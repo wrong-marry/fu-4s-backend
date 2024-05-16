96e7d1e6bd711d5ddd9e289aba70953ce044eb0e
Phan Trung Dũng <aninatsumi@gmail.com>
2024-05-16 15:46:27 +0700

    init project

---
+++ src/main/java/core/fu4sbackend/Fu4sBackendApplication.java
@@ -0,0 +1,13 @@
+package core.fu4sbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fu4sBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fu4sBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/fu4sbackend/Fu4sBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package core.fu4sbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fu4sBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
