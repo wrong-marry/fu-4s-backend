7d683e75f26ca254bd6839cc987c824fdd6007db
Phan Trung Dũng <aninatsumi@gmail.com>
2024-05-18 14:40:10 +0700

    update notification messages

--- src/main/java/core/fu4sbackend/constant/NotificationMessage.java
+++ src/main/java/core/fu4sbackend/constant/NotificationMessage.java
@@ -1,5 +1,14 @@
 package core.fu4sbackend.constant;
 
-public enum NotificationMessage {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public enum NotificationMessage {
+    APPROVED_POST("Your post have been approved"),
+    DISAPPROVED_POST("Your post has been disapproved"),
+    HIDE_COMMENT("Your comment has been hidden");
+
+    private final String message;
 }
