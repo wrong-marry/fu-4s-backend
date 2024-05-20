a120bac4c3cbba38675a388e9b2d2c3a12dfacfd
Phan Trung Dũng <aninatsumi@gmail.com>
2024-05-20 14:20:44 +0700

    fix primary key type in repository

--- src/main/java/core/fu4sbackend/repository/SubjectRepository.java
+++ src/main/java/core/fu4sbackend/repository/SubjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+public interface SubjectRepository extends JpaRepository<Subject, String> {
 }
