db71f8cc62d8b6a331bb847bf66e9479d58edd52
Phan Trung Dũng <aninatsumi@gmail.com>
2024-05-20 14:41:25 +0700

    fix dto builder

--- src/main/java/core/fu4sbackend/dto/PostDto.java
+++ src/main/java/core/fu4sbackend/dto/PostDto.java
@@ -2,14 +2,17 @@ package core.fu4sbackend.dto;
 
 import core.fu4sbackend.constant.PostStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class PostDto {
     private Integer id;
     private Date postTime;
