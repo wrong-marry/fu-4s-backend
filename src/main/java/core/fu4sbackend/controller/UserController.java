3fd980ae95b8118e11756ad6dbabe07ce13dd111
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2024-05-23 16:15:58 +0700

    add user controller and service

---
+++ src/main/java/core/fu4sbackend/controller/UserController.java
@@ -0,0 +1,22 @@
+package core.fu4sbackend.controller;
+import core.fu4sbackend.dto.UserDto;
+import core.fu4sbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public ResponseEntity<UserDto> getByUsername(@RequestParam("username") String username){
+        return new ResponseEntity<>(
+        userService.getByUsername(username), HttpStatus.OK);
+    }
+}
