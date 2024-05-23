da651a2c4ee22f00ea18469f5c782914fcbcfa4f
phamkimthoa <kimthoa16042004@gmail.com>
2024-05-23 20:23:45 +0700

    add : model mapping ver2

--- src/main/java/core/fu4sbackend/service/QuestionSetService.java
+++ src/main/java/core/fu4sbackend/service/QuestionSetService.java
@@ -2,8 +2,6 @@ package core.fu4sbackend.service;
 
 import core.fu4sbackend.dto.QuestionSetDto;
 import core.fu4sbackend.entity.QuestionSet;
-import core.fu4sbackend.repository.QuestionRepository;
-import core.fu4sbackend.repository.QuestionSetRepository;
 import core.fu4sbackend.repository.QuestionSetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +9,11 @@ import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionSetService {
+
     private QuestionSetRepository questionSetRepository;
 
     @Autowired
@@ -32,10 +32,17 @@ public class QuestionSetService {
         List<QuestionSetDto> questionSetDtos = new ArrayList<>();
 
         ModelMapper modelMapper = new ModelMapper();
-        for (QuestionSet questionSet : questionSets) {
-            QuestionSetDto questionSetDto = modelMapper.map(questionSet, QuestionSetDto.class);
-            questionSetDtos.add(questionSetDto);
-        }
+        questionSetDtos = questionSets
+                .stream()
+                .map(questionSet -> {
+                    QuestionSetDto questionSetDto =  modelMapper.map(questionSet, QuestionSetDto.class);
+                    questionSetDto.setUsername(questionSet.getUser().getFirstName()+" "+questionSet.getUser().getLastName());
+
+                    return questionSetDto ;
+                })
+
+                .collect(Collectors.toList());
+
 
         return questionSetDtos;
     }
