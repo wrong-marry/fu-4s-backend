59c602e76a34e0e0d1771f879d8c8b57b8ec14da
Quynh4 <nofomtre@gmail.com>
2024-05-24 13:54:08 +0700

    update LearningMaterialController

--- src/main/java/core/fu4sbackend/controller/LearningMaterialController.java
+++ src/main/java/core/fu4sbackend/controller/LearningMaterialController.java
@@ -1,17 +1,13 @@
 package core.fu4sbackend.controller;
 
+
 import core.fu4sbackend.dto.LearningMaterialDto;
-import core.fu4sbackend.dto.SearchRequest;
-import core.fu4sbackend.entity.LearningMaterial;
-import core.fu4sbackend.entity.Post;
 import core.fu4sbackend.service.LearningMaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,11 +24,6 @@ public class LearningMaterialController {
     public List<LearningMaterialDto> getAllLearningMaterials(){
         return learningMaterialService.getAllLearningMaterials();
     }
-
-//    @GetMapping("/getAllLearningMaterials")
-//    public ResponseEntity<String> getAllLearningMaterials(){
-//        return ResponseEntity.ok(learningMaterialService.getAllLearningMaterials().toString()) ;
-//    }
 }
 
 
