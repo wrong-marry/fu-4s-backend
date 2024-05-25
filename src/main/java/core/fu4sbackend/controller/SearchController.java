add61456597cc5270ac07f786121a1550fd989f1
Hai Vu <vmhtl2004@gmail.com>
2024-05-25 12:28:58 +0700

    Modify: Search controller (fix request mapping)

--- src/main/java/core/fu4sbackend/controller/SearchController.java
+++ src/main/java/core/fu4sbackend/controller/SearchController.java
@@ -10,12 +10,14 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1/search")
 public class SearchController {
     private final LearningMaterialService learningMaterialService;
     private final QuestionSetService questionSetService;
@@ -28,11 +30,11 @@ public class SearchController {
         this.postService = postService;
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<String> search(@RequestParam String keyword) {
+    @GetMapping("")
+    public ResponseEntity<String> searchPost(@RequestParam(required = false) String keyword) {
         List<LearningMaterialDto> materialList = learningMaterialService.findByKeyword(keyword);
 
-        SearchRequest sr = new SearchRequest(null,keyword,null,null,true);
+        SearchRequest sr = new SearchRequest(null,keyword,null,null,null);
         List<PostDto> questionList = postService.findAllByCriteria(sr);
         JSONObject jsonObject = new JSONObject(List.of(materialList, questionList));
         jsonObject.put("learningMaterials", materialList);
