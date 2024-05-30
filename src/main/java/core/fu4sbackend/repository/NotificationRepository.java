3f9d45380bba8086434f8aeca47f907f8dfe3f88
Quynh4 <nofomtre@gmail.com>
2024-05-30 16:41:11 +0700

    update NotificationRepository

--- src/main/java/core/fu4sbackend/repository/NotificationRepository.java
+++ src/main/java/core/fu4sbackend/repository/NotificationRepository.java
@@ -2,8 +2,13 @@ package core.fu4sbackend.repository;
 
 import core.fu4sbackend.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    @Query("select n from Notification n where n.user.username = ?1")
+    List<Notification> getAllByUsername(String username);
 }
