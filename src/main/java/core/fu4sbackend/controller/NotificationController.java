c004ad5f9f2d5df9a866a8d4e312c7adb4f48017
Quynh4 <nofomtre@gmail.com>
2024-05-30 17:08:44 +0700

    update NotificationController

--- src/main/java/core/fu4sbackend/controller/NotificationController.java
+++ src/main/java/core/fu4sbackend/controller/NotificationController.java
@@ -1,12 +1,11 @@
 package core.fu4sbackend.controller;
 
 import core.fu4sbackend.dto.NotificationDto;
+import core.fu4sbackend.dto.PostDto;
 import core.fu4sbackend.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +25,10 @@ public class NotificationController {
     public List<NotificationDto> getAllNotifications() {
         List<NotificationDto> notifications = notificationService.getAllNotificationDtos();
         return notifications;
+    }
+
+    @GetMapping("/getAllByUsername")
+    public ResponseEntity<List<NotificationDto>> showAllNotificationsByUsername(@RequestParam String username) {
+        return ResponseEntity.ok(notificationService.getAllByUsername(username));
     }
 }
