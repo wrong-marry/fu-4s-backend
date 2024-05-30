8162c6c2f964229021e08ed5a5d4e8833b08e5e4
Hai Vu <vmhtl2004@gmail.com>
2024-05-30 15:32:15 +0700

    Add: upload comments

--- src/main/java/core/fu4sbackend/controller/CommentController.java
+++ src/main/java/core/fu4sbackend/controller/CommentController.java
@@ -2,13 +2,11 @@ package core.fu4sbackend.controller;
 
 import core.fu4sbackend.dto.CommentDto;
 import core.fu4sbackend.service.CommentService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/comments")
 public class CommentController {
     private final CommentService commentService;
+
     @Autowired
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
@@ -24,5 +23,21 @@ public class CommentController {
     @GetMapping("/post/{id}")
     public ResponseEntity<List<CommentDto>> getCommentsInAPost(@PathVariable int id) {
         return new ResponseEntity<>(commentService.findByPostId(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/upload/post-{id}")
+    public ResponseEntity<String> uploadComment(@PathVariable int id, @RequestBody CommentDto commentDto) {
+        JSONObject jsonObject = new JSONObject();
+        switch (commentService.save(commentDto, id)) {
+            case -1:
+                jsonObject.put("message", "Invalid username");
+                return new ResponseEntity<>(jsonObject.toString(), HttpStatus.CONFLICT);
+            case -2:
+                jsonObject.put("message", "Invalid post id");
+                return new ResponseEntity<>(jsonObject.toString(), HttpStatus.CONFLICT);
+            default:
+                jsonObject.put("message", "Successfully uploaded comment");
+                return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
+        }
     }
 }
--- src/main/java/core/fu4sbackend/service/CommentService.java
+++ src/main/java/core/fu4sbackend/service/CommentService.java
@@ -1,21 +1,31 @@
 package core.fu4sbackend.service;
 
+import core.fu4sbackend.constant.CommentStatus;
 import core.fu4sbackend.dto.CommentDto;
 import core.fu4sbackend.entity.Comment;
+import core.fu4sbackend.entity.Post;
+import core.fu4sbackend.entity.User;
 import core.fu4sbackend.repository.CommentRepository;
+import core.fu4sbackend.repository.PostRepository;
+import core.fu4sbackend.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CommentService {
+    private UserRepository userRepository;
+    private PostRepository postRepository;
     private CommentRepository commentRepository;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(UserRepository userRepository, PostRepository postRepository, CommentRepository commentRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
         this.commentRepository = commentRepository;
     }
 
@@ -25,7 +35,7 @@ public class CommentService {
         Comment comment = commentRepository.findById(id).orElse(null);
         if (comment == null) return null;
         CommentDto commentDto = modelMapper.map(comment, CommentDto.class);
-        commentDto.setUsername(comment.getUser().getFirstName()+comment.getUser().getLastName());
+        commentDto.setUsername(comment.getUser().getFirstName() + comment.getUser().getLastName());
         return commentDto;
     }
 
@@ -35,8 +45,24 @@ public class CommentService {
         List<Comment> comments = commentRepository.findByPostId(postId);
         return comments.stream().map(comment -> {
             CommentDto commentDto = modelMapper.map(comment, CommentDto.class);
-            commentDto.setUsername(comment.getUser().getFirstName()+comment.getUser().getLastName());
+            commentDto.setUsername(comment.getUser().getFirstName() + comment.getUser().getLastName());
             return commentDto;
         }).toList();
+    }
+
+    @Transactional
+    public int save(CommentDto commentDto, Integer postId) {
+        Comment c = new Comment();
+        User u = userRepository.findByUsername(commentDto.getUsername()).orElse(null);
+        Post p = postRepository.findById(postId).orElse(null);
+        if (u == null) return -1;
+        if (p == null) return -2;
+        c.setUser(u);
+        c.setPost(p);
+        c.setContent(commentDto.getContent());
+        c.setDate(commentDto.getDate());
+        c.setStatus(CommentStatus.ACTIVE);
+        commentRepository.save(c);
+        return 0;
     }
 }
