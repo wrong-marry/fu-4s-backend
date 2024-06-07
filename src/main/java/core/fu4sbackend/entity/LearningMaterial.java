6904ec9fd1e473d6715ded41806cbadf723153e9
Phan Trung Dũng <aninatsumi@gmail.com>
2024-06-07 16:57:38 +0700

    rename: change file name

--- src/main/java/core/fu4sbackend/entity/LearningMaterial.java
+++ src/main/java/core/fu4sbackend/entity/LearningMaterial.java
@@ -2,12 +2,10 @@ package core.fu4sbackend.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Getter
@@ -20,5 +18,5 @@ public class LearningMaterial extends Post {
     private String content;
 
     @OneToMany(mappedBy = "learningMaterial", cascade = CascadeType.ALL)
-    private Collection<MaterialImage> images;
+    private Collection<MaterialFile> images;
 }
--- src/main/java/core/fu4sbackend/repository/MaterialFileRepository.java
+++ src/main/java/core/fu4sbackend/repository/MaterialFileRepository.java
@@ -1,9 +1,9 @@
 package core.fu4sbackend.repository;
 
-import core.fu4sbackend.entity.MaterialImage;
+import core.fu4sbackend.entity.MaterialFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaterialImageRepository extends JpaRepository<MaterialImage, Integer> {
+public interface MaterialFileRepository extends JpaRepository<MaterialFile, Integer> {
 }
