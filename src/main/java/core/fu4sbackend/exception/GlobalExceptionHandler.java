69b63bedecb1638ff378228bcf98d6c3f7e309e7
vnminhhai <vmhtl2004@gmail.com>
2024-06-24 15:29:00 +0700

    Add: Exception handlers

--- src/main/java/core/fu4sbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/core/fu4sbackend/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package core.fu4sbackend.exception;
 
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,8 +18,11 @@ public class GlobalExceptionHandler {
         if (exception instanceof BadCredentialsException) {
             return new ResponseEntity<>("Bad credentials", HttpStatus.UNAUTHORIZED);
         }
-        else if (exception instanceof HttpClientErrorException.Unauthorized) {
-            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        else if (exception instanceof MalformedJwtException) {
+            return new ResponseEntity<>("Invalid token", HttpStatus.UNAUTHORIZED);
+        }
+        else if (exception instanceof NoSuchElementException) {
+            return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
