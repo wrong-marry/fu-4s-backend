cf59be4e78156eb819586311a2106ee5c440872e
phamkimthoa <kimthoa16042004@gmail.com>
2024-07-19 13:28:36 +0700

    add DirectNotification

---
+++ src/main/java/core/fu4sbackend/dto/DirectNotificationDto.java
@@ -0,0 +1,18 @@
+package core.fu4sbackend.dto;
+
+import core.fu4sbackend.constant.NotificationMessage;
+import core.fu4sbackend.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DirectNotificationDto {
+    private NotificationMessage message;
+    private String username ;
+
+}
