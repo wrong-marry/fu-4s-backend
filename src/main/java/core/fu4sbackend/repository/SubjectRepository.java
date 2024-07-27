5d44edbdcfce17393b0b69a16be58e85cddaa67f
Quynh4 <nofomtre@gmail.com>
2024-07-27 23:20:54 +0700

    getAllActive subject

--- src/main/java/core/fu4sbackend/repository/SubjectRepository.java
+++ src/main/java/core/fu4sbackend/repository/SubjectRepository.java
@@ -2,6 +2,7 @@ package core.fu4sbackend.repository;
 
 import core.fu4sbackend.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,10 @@ import java.util.List;
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, String> {
     public List<Subject> findAllByOrderBySemesterAsc();
+
+
+    @Query("SELECT s FROM Subject s WHERE s.isActive = true ORDER BY s.semester ASC")
+    List<Subject> findAllActiveOrderBySemesterAsc();
 
     Integer countByIsActive(boolean isActive);
 
